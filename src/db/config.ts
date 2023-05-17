846e06d2d6d044558a08abb49d3110dcbbbf8ffd
hamadazaid2 <hamadazaid62@gmail.com>
2023-05-17 09:39:37 +0300

    last commit

--- src/db/config.ts
+++ src/db/config.ts
@@ -1,23 +1,3 @@
-module.exports = {
-  development: {
-    username: 'root',
-    password: null,
-    database: 'to_do_list',
-    host: '127.0.0.1',
-    dialect: 'mysql',
-  },
-  test: {
-    username: 'root',
-    password: null,
-    database: 'to_do_list',
-    host: '127.0.0.1',
-    dialect: 'mysql',
-  },
-  production: {
-    username: 'root',
-    password: null,
-    database: 'to_do_list',
-    host: '127.0.0.1',
-    dialect: 'mysql',
-  },
-};
+import config from '../../config';
+
+module.exports = config().database;
